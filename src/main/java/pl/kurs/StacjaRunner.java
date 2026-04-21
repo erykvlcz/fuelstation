50dc89144380d30870a7ce5156f69cd6b504b860
Eryk Kowalczyk <kowalczykeryk@op.pl>
2026-04-21 03:46:03 +0200

    first commit

---
+++ src/main/java/pl/kurs/StacjaRunner.java
@@ -0,0 +1,12 @@
+package pl.kurs;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class StacjaRunner {
+//    ExecutorService executorService = Executors.newFixedThreadPool(2);
+//
+//    StacjaBenzynowa stacjaBenzynowa = new StacjaBenzynowa(new Dystrybutor("Diesel"), executorService);
+//
+//        stacjaBenzynowa.rozpocznijTankowanie(4);
+}
