bf5c3e0c4776a0b8ee0cbbd66f5b0c338dedcaa6
Eryk Kowalczyk <kowalczykeryk@op.pl>
2026-04-21 03:48:51 +0200

    classes edited

--- src/main/java/pl/kurs/Dystrybutor.java
+++ src/main/java/pl/kurs/Dystrybutor.java
@@ -1,4 +1,20 @@
 package pl.kurs;
 
 public class Dystrybutor {
+    private String id;
+
+    public Dystrybutor(String id) {
+        this.id = id;
+    }
+
+    public void zatankuj(){
+        System.out.println("Tankowanie paliwa");
+    }
+
+    @Override
+    public String toString() {
+        return "Dystrybutor{" +
+                "id='" + id + '\'' +
+                '}';
+    }
 }
--- src/main/java/pl/kurs/StacjaRunner.java
+++ src/main/java/pl/kurs/StacjaRunner.java
@@ -4,9 +4,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class StacjaRunner {
-//    ExecutorService executorService = Executors.newFixedThreadPool(2);
-//
-//    StacjaBenzynowa stacjaBenzynowa = new StacjaBenzynowa(new Dystrybutor("Diesel"), executorService);
-//
-//        stacjaBenzynowa.rozpocznijTankowanie(4);
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        StacjaBenzynowa stacjaBenzynowa = new StacjaBenzynowa(new Dystrybutor("Diesel"), executorService);
+
+        stacjaBenzynowa.rozpocznijTankowanie(4);
+    }
 }
